1d3f9deb7485dd6d1cc53bb656d2ff896aa967c3
vagrant <lowbere@yahoo.com>
2014-12-17 11:28:28 +0000

    added edit functionality to wikis.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,6 +4,7 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki =Wiki.new
   end
 
   def show
@@ -11,5 +12,29 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error updating the wiki."
+      render :edit
+    end
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :new
+    end 
   end
 end
